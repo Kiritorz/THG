49e267ba25bb090e6c46745e5d8a24ec4d0d9a8d
k1ssinnlee <948655078@qq.com>
2024-08-08 23:45:49 +0800

    fix:..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "THOG",
+  title: "THG",
   description: "Texas Hold'em Poker Card Game",
 };
 
